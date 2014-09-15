7dbb5c709fc705c8b1b8d8a08ea80a8c98213118
Happy S Enchill <henchill@mit.edu>
2014-09-15 13:00:57 -0400

    made sure static files worked on scripts.mit.edu

--- ccity/urls.py
+++ ccity/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -16,5 +17,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', 'ccity.views.index', name='index')
+    url(r'^$', 'ccity.views.index', name='index'),
+
+
 )
